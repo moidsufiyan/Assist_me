64c3d602bf4f99cee9ffadf434a86361ced22d13
moidsufiyan <mmoidsufiyan4151@gmail.com>
2026-03-22 14:05:41 +0530

    refractor for render deployment

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:5173';
+const CLIENT_URL = (process.env.CLIENT_URL || 'http://localhost:5173').replace(/\/$/, '');
 
 app.use(cors({
   origin: CLIENT_URL,
