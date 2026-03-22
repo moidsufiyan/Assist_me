788636c4d3bf219f9c9bde8cde1cbb3b06bc4e4b
moidsufiyan <mmoidsufiyan4151@gmail.com>
2026-03-22 12:55:20 +0530

    configured axios withCredentials to support cookie transmission sets

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,30 +1,14 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api'
+  baseURL: import.meta.env.VITE_API_URL || '/api',
+  withCredentials: true
 });
-
-
-api.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  }
-);
-
 
 api.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response && error.response.status === 401) {
-      localStorage.removeItem('token');
-      localStorage.removeItem('user');
       window.location.href = '/login';
     }
     return Promise.reject(error);
